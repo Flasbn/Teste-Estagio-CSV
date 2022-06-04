17fbab32a91eeea75c6024cc22aaeb7bd6bb6078
Flávio Sabino <flavio.08@hotmail.com>
2022-06-03 21:39:10 -0300

    Primeiros passos
    Levantamento de arquivos

---
+++ script.js
@@ -0,0 +1,10 @@
+const bandeira = [
+    "Rosa",
+    "Azul",
+    "Roxo",
+    "Verde",
+    "Vermelho",
+    "Amarelo"
+];
+
+console.log(bandeira[bandeira.length -1]);
---
+++ teste.py
@@ -0,0 +1,3 @@
+print('Hello world')
+nome = 'Flávio'
+print(idade)
